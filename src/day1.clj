7e3937fd30a92bda862b0c26ec36e7960421ec45
Ryan Baker <mail@rlbaker.net>
2022-12-01 09:51:12 -0800

    cleanup

--- src/day1.clj
+++ src/day1.clj
@@ -1,14 +1,15 @@
 (ns day1
   (:require advent))
 
-; (def input (advent/groups "day1.example"))
-(def input (advent/groups "day1.input"))
-
 (defn to-ints [coll] (map parse-long coll))
 (defn sum-ints [coll] (map #(reduce + (to-ints %)) coll))
 (defn top-n [n coll] (take-last n (sort coll)))
+
 (defn part1 [input] (apply + (top-n 1 (sum-ints input))))
 (defn part2 [input] (apply + (top-n 3 (sum-ints input))))
+
+; (def input (advent/groups "day1.example"))
+(def input (advent/groups "day1.input"))
 
 (println (part1 input))
 (println (part2 input))
