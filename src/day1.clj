daf91f2d22cff72d7bfcf0787f6d314ab54074a3
Ryan Baker <mail@rlbaker.net>
2022-12-02 10:27:07 -0800

    day2 cleanup

--- src/day1.clj
+++ src/day1.clj
@@ -1,15 +1,15 @@
-(ns day1
-  (:require advent clojure.pprint))
+(ns day1 (:require advent))
+
+(defn sum-cals [group] (reduce + (map parse-long group)))
+(defn sum-groups [groups] (map sum-cals groups))
+(defn top-n [n cals] (take-last n (sort coll)))
+(defn sum-top [n cals] (apply + (top-n n coll)))
+
+(defn part1 [input] (sum-top 1 (sum-groups input)))
+(defn part2 [input] (sum-top 3 (sum-groups input)))
 
 (def example (advent/groups "day1.example"))
 (def input (advent/groups "day1.input"))
-
-(defn to-ints [coll] (map parse-long coll))
-(defn sum-cals [coll] (map #(reduce + (to-ints %)) coll))
-(defn top-n [n coll] (take-last n (sort coll)))
-
-(defn part1 [input] (apply + (top-n 1 (sum-cals input))))
-(defn part2 [input] (apply + (top-n 3 (sum-cals input))))
 
 (println (part1 input))
 (println (part2 input))
