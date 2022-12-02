bc6a43e3cb1c42a209009a25243a6eef562c8f7d
Ryan Baker <mail@rlbaker.net>
2022-12-02 12:53:31 -0800

    cleanup

--- src/advent.clj
+++ src/advent.clj
@@ -2,6 +2,7 @@
 
 (defn split-fields [s] (str/split s #" "))
 (defn split-groups [s] (str/split s #"\n\n"))
+
 (defn lines [input] (str/split-lines input))
 (defn groups [input] (map str/split-lines (split-groups input)))
 (defn fields [input] (map split-fields (str/split-lines input)))
