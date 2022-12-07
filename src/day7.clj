255ff1ea8639329e3f9b283147ca21e54f2fd4a7
Ryan Baker <mail@rlbaker.net>
2022-12-07 12:03:20 -0800

    cleanup

--- src/day7.clj
+++ src/day7.clj
@@ -7,39 +7,51 @@
   [lines]
   (map #(str/split % #" ") lines))
 
-(defn parse-cmds [[input & output]]
+(defn parse-cmds
+  [[input & output]]
   (let [[cmd path] input]
     (cond
       (= cmd "cd") [:cd path]
       (= cmd "ls") (conj output :ls))))
 
-(defn parse-lines [input]
+(defn parse-lines
+  [input]
   (->> (str/split input #"\$ ")
        (drop 1)
        (map str/split-lines)
        (map split)))
 
-(defn update-pos [pos [dir]]
+(defn update-pos
+  [pos [dir]]
   (if (= dir "..")
     (pop pos)
     (conj pos dir)))
 
 (defn no-dirs [[info _]] (not= info "dir"))
 
-(defn update-fs [fs pos data]
+(defn update-fs
+  [fs pos data]
   (->> data
        (filter no-dirs)
        (map #(vector pos (parse-long (first %))))
        (reduce concat fs)))
 
-(defn parse-fs [cmds]
+(defn parse-fs
+  [cmds]
   (loop [fs []
          pos []
          [[cmd & data] & cmds] cmds]
     (cond
-      (= 0 (count cmds)) (partition 2 fs)
-      (= cmd :cd) (let [pos (update-pos pos data)] (recur fs pos cmds))
-      (= cmd :ls) (let [fs (update-fs fs pos data)] (recur fs pos cmds)))))
+      (= 0 (count cmds))
+      (partition 2 fs)
+
+      (= cmd :cd)
+      (let [pos (update-pos pos data)]
+        (recur fs pos cmds))
+
+      (= cmd :ls)
+      (let [fs (update-fs fs pos data)]
+        (recur fs pos cmds)))))
 
 (defn update-sums
   [sums dir size]
@@ -62,7 +74,8 @@
        (parse-fs)
        (fs-sum)))
 
-(defn sum-smallest [sum [_ size]]
+(defn sum-smallest
+  [sum [_ size]]
   (if (< size 100000)
     (+ sum size)
     sum))
